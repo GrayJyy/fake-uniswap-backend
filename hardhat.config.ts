ae3dc124867f656b3d556bf755c11cc091bf43b6
GrayJiang <Gray_Jy@yeah.net>
2023-06-02 22:03:05 +0800

    feat: add mainnet config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,6 +9,7 @@ import { GAS, GAS_PRICE } from './constants/constant'
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY!
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
 useProxyAgent()
 
 const config: HardhatUserConfig = {
@@ -25,6 +26,7 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: { chainId: 31337 },
     sepolia: { chainId: 11155111, url: SEPOLIA_RPC_URL, accounts: [PRIVATE_KEY], gasPrice: GAS_PRICE, gas: GAS },
+    mainnet: { chainId: 1, url: MAINNET_RPC_URL, accounts: [PRIVATE_KEY], gasPrice: GAS_PRICE, gas: GAS },
   },
   namedAccounts: { deployer: { default: 0, 1: 0 }, user: { default: 1 } },
   etherscan: { apiKey: ETHERSCAN_API_KEY },

a30c7921ea22313d3c006cea8c974577b3885778
GrayJiang <Gray_Jy@yeah.net>
2023-06-05 23:23:20 +0800

    feat: add multiSwap and test

--- deploy/multiSwapTokenDeploy.ts
+++ deploy/multiSwapTokenDeploy.ts
@@ -3,23 +3,23 @@ import { WAIT_CONFIRMATIONS } from '../constants/constant'
 import { developChain } from '../helperHardhatConfig'
 import verify from '../utils/verify'
 
-const swapTokenDeploy: DeployFunction = async ({ deployments, getNamedAccounts, network }) => {
+const multiSwapTokenDeploy: DeployFunction = async ({ deployments, getNamedAccounts, network }) => {
   const { deploy, log } = deployments
   const { deployer } = await getNamedAccounts()
   const args = ['0xE592427A0AEce92De3Edee1F18E0157C05861564']
   const waitConfirmations = developChain.includes(network.name) ? 1 : WAIT_CONFIRMATIONS
-  const swapToken = await deploy('SwapToken', {
+  const multiSwapToken = await deploy('SwapMultihop', {
     from: deployer,
     args,
     log: true,
     waitConfirmations,
   })
-  log(`Deployed swapToken contract at address: ${swapToken.address} ,the network is ${network.name}`)
+  log(`Deployed multiSwapToken contract at address: ${multiSwapToken.address} ,the network is ${network.name}`)
   log('---------------------------------------------------------------------------------------------')
   if (!developChain.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
-    verify(swapToken.address, args)
+    verify(multiSwapToken.address, args)
   }
 }
 
-swapTokenDeploy.tags = ['all', 'swaptoken']
-export default swapTokenDeploy
+multiSwapTokenDeploy.tags = ['all', 'swapmultihop']
+export default multiSwapTokenDeploy
--- test/unit/multiSwapToken.ts
+++ test/unit/multiSwapToken.ts
@@ -1,14 +1,15 @@
 import { deployments, ethers, getNamedAccounts, network } from 'hardhat'
 import { DAI_ADDRESS, WETH9_ADDRESS, USDC_ADDRESS } from '../../constants/constant'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
-import { IWeth, SwapToken } from '../../typechain-types'
+import { IWeth } from '../../typechain-types'
 import { developChain } from '../../helperHardhatConfig'
 import { assert, expect } from 'chai'
+import { SwapMultihop } from '../../typechain-types/contracts/SwapMultihop'
 
 !developChain.includes(network.name)
   ? describe.skip
-  : describe('SwapToken', () => {
-      let swapToken: SwapToken
+  : describe('SwapMultihop', () => {
+      let swapMultihop: SwapMultihop
       let signer: SignerWithAddress
       let weth: IWeth
       let dai: IWeth
@@ -16,34 +17,34 @@ import { assert, expect } from 'chai'
       beforeEach(async () => {
         const { deployer, user } = await getNamedAccounts()
         signer = await ethers.getSigner(deployer)
-        await deployments.fixture(['swaptoken'])
-        swapToken = await ethers.getContract('SwapToken')
+        await deployments.fixture(['swapmultihop'])
+        swapMultihop = await ethers.getContract('SwapMultihop')
         weth = await ethers.getContractAt('IWeth', WETH9_ADDRESS)
         dai = await ethers.getContractAt('IWeth', DAI_ADDRESS)
         usdc = await ethers.getContractAt('IWeth', USDC_ADDRESS)
       })
       describe('constructor', () => {
         it('Should gets the correct swapRouter', async () => {
-          const swapRouter = await swapToken.swapRouter()
+          const swapRouter = await swapMultihop.swapRouter()
           assert.equal(swapRouter, '0xE592427A0AEce92De3Edee1F18E0157C05861564')
         })
       })
 
-      describe('swapExactInputSingle', () => {
+      describe('swapExactInputMultihop', () => {
         it('Should gets the dai token and reset all weth token correctly', async () => {
           const amount = ethers.utils.parseEther('0.01')
           await weth.deposit({ value: amount })
-          await weth.approve(swapToken.address, amount)
-          await swapToken.swapExactInputSingle(amount)
+          await weth.approve(swapMultihop.address, amount)
+          await swapMultihop.swapExactInputMultihop(amount)
         })
       })
-      describe('swapExactOutputSingle', () => {
+      describe('swapExactOutputMultihop', () => {
         it('Should output the certain dai amount and refund the remain weth', async () => {
           const amountInMaximum = ethers.utils.parseEther('0.1')
           await weth.deposit({ value: amountInMaximum })
-          await weth.approve(swapToken.address, amountInMaximum)
+          await weth.approve(swapMultihop.address, amountInMaximum)
           const amountOut = ethers.utils.parseEther('1')
-          await swapToken.swapExactOutputSingle(amountOut, amountInMaximum)
+          await swapMultihop.swapExactOutputMultihop(amountOut, amountInMaximum)
         })
       })
     })
